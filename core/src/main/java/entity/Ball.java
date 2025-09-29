2f047672cbc35a69f91afd514cb9419cf1853f07
hkhoi23 <haikhoi2006@gmail.com>
2025-09-29 10:39:15 +0700

    add new variable

--- core/src/main/java/entity/Ball.java
+++ core/src/main/java/entity/Ball.java
@@ -3,7 +3,7 @@ package entity;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Ball extends MovableObject {
-
+    boolean checkCollision;
     public Ball(int x, int y, int w, int h, Texture texture) {
         super(x, y, w, h, texture);
     }
