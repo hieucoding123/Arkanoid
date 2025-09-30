8002af02b9ef9462fc270b3bb6d11d64311f022e
hieucoding123 <phamhieu211206@gmail.com>
2025-10-01 01:46:12 +0700

    feat(GamObject): add scale attribute and save original size of object

--- core/src/main/java/entity/GameObject.java
+++ core/src/main/java/entity/GameObject.java
@@ -7,8 +7,9 @@ import com.badlogic.gdx.math.Rectangle;
 public abstract class GameObject {
     protected float x;
     protected float y;
-    protected float width;
-    protected float height;
+    protected float orgWidth;       // original width
+    protected float orgHeight;      // original height
+    protected float scale;
 
     private final Texture texture;
 
@@ -21,10 +22,29 @@ public abstract class GameObject {
      * @param texture texture contains image of object
      */
     public GameObject(float x, float y, float w, float h, Texture texture) {
+        this.scale = 1;
         this.x = x;
         this.y = y;
-        this.width = w;
-        this.height = h;
+        this.orgWidth = w;
+        this.orgHeight = h;
+        this.texture = texture;
+    }
+
+    /**
+     * Initialize game object.
+     * @param x x coordinate of the object
+     * @param y y coordinate of the object
+     * @param w width of the rectangle containing the object
+     * @param h height of the rectangle containing the object
+     * @param scale scale of object
+     * @param texture texture contains image of object
+     */
+    public GameObject(float x, float y, float w, float h, float scale, Texture texture) {
+        this.x = x;
+        this.y = y;
+        this.orgWidth = w;
+        this.orgHeight = h;
+        this.scale = scale;
         this.texture = texture;
     }
 
@@ -32,13 +52,13 @@ public abstract class GameObject {
      *Bounding box to check for collisions.
      */
     public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
+        return new Rectangle(x, y, this.orgWidth * this.scale, this.orgHeight * this.scale);
     }
     /**
      * Draw object on game screen.
      * @param batch game drawing programming
      */
     public void draw(SpriteBatch batch) {
-        batch.draw(texture, x, y, width, height);
+        batch.draw(texture, x, y, this.orgWidth * this.scale, this.orgHeight * this.scale);
     }
 }
