37bc058a833c7098f741156b1a0e463403ebc337
hieucoding123 <phamhieu211206@gmail.com>
2025-10-04 00:20:22 +0700

    feat(effectitem): implement static methods for managing effect items

--- core/src/main/java/entity/EffectItem.java
+++ core/src/main/java/entity/EffectItem.java
@@ -1,8 +1,12 @@
 package entity;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
 
 public abstract class EffectItem extends MovableObject{
+    private static ArrayList<EffectItem> items = new ArrayList<EffectItem>();
     private long startTime;
 
     public EffectItem(float x, float y, float dy, Texture texture) {
@@ -10,6 +14,7 @@ public abstract class EffectItem extends MovableObject{
         this.dx = 0;            // can only drop down
         this.dy = dy;
         this.startTime = System.currentTimeMillis();
+        items.add(this);
     }
 
     public EffectItem(float x, float y, float dy, float scale, Texture texture) {
@@ -17,7 +22,43 @@ public abstract class EffectItem extends MovableObject{
         this.dx = 0;
         this.dy = dy;           // can only drop down
         this.startTime = System.currentTimeMillis();
+        items.add(this);
     }
 
+    /**
+     * Apply effect.
+     */
     public abstract void applyEffect();
+
+    /**
+     * Add a new effect item to list.
+     * @param effectItem EffectItem object
+     */
+    public static void addEffectItem(EffectItem effectItem) {
+        items.add(effectItem);
+    }
+
+    /**
+     * Update all effect items and check collision with paddle.
+     */
+    public static void updateEffectItems(Paddle paddle) {
+        for (EffectItem effectItem : items) {
+            effectItem.update();
+            if (paddle.checkCollision(effectItem)) {
+                effectItem.applyEffect();
+                effectItem.setDestroyed(true);
+            }
+        }
+        EffectItem.items.removeIf(EffectItem::isDestroyed);
+    }
+
+    /**
+     * Draw all effect items on game screen.
+     * @param batch SpriteBatch object
+     */
+    public static void drawEffectItems(SpriteBatch batch) {
+        for (EffectItem effectItem : items) {
+            effectItem.draw(batch);
+        }
+    }
 }
