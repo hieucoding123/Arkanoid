6fea97d5f7d1a5eeef7c0a9011a5060e3c2514e9
QLinhhhh <24020201@vnu.edu.vn>
2025-10-04 14:33:57 +0700

    feat(ball): update function and change use direction to angle

--- core/src/main/java/entity/Ball.java
+++ core/src/main/java/entity/Ball.java
@@ -5,19 +5,58 @@ import com.badlogic.gdx.Gdx;
 
 public class Ball extends MovableObject {
     private float speed;
-    private float directionX;
-    private float directionY;
+    private float angle;
 
     public Ball(float x, float y, float w, float h, Texture texture, float speed) {
         super(x, y, w, h, texture);
         this.speed = speed;
-        this.directionX = 1;
-        this.directionY = 1;
+        setRandomAngle();
+    }
+
+    private void setRandomAngle() {
+        this.angle = (float) (Math.random() * (Math.PI / 2) + (Math.PI / 4));
     }
 
     public void updateVelocity() {
-        this.dx = speed * directionX;
-        this.dy = speed * directionY;
+        this.dx = speed * (float)Math.cos(angle);
+        this.dy = speed * (float)Math.sin(angle);
+    }
+
+
+    public void reverseX() {
+        // Để phản xạ qua trục Y (tường trái/phải), góc mới = PI - góc cũ
+        this.angle = (float)Math.PI - this.angle;
+        updateVelocity();
+    }
+
+
+    public void reverseY() {
+        // Để phản xạ qua trục X (tường trên/dưới), góc mới = -góc cũ
+        this.angle = -this.angle;
+        updateVelocity();
+    }
+
+    public void setAngle(float angleInRadians) {
+        this.angle = angleInRadians;
+        updateVelocity();
+    }
+
+    public float getAngle() {
+        return this.angle;
+    }
+
+    public void setSpeed(float newSpeed) {
+        this.speed = newSpeed;
+        updateVelocity();
+    }
+
+
+    public void reset() {
+        this.x = (Gdx.graphics.getWidth() / 2f) - (this.getWidth() / 2f);
+        this.y = 120;
+        setRandomAngle();
+        this.dx = 0;
+        this.dy = 0;
     }
 
     public void bounceOff(GameObject other) {
