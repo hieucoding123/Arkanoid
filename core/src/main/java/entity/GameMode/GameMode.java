829f57ed18bd60d5c7095d58a12afd49a5633c16
hieucoding123 <phamhieu211206@gmail.com>
2025-10-17 00:36:38 +0700

    feat(gamemode): add attributes and methods of gamemode class

--- core/src/main/java/entity/GameMode/GameMode.java
+++ core/src/main/java/entity/GameMode/GameMode.java
@@ -1,4 +1,18 @@
 package entity.GameMode;
 
-public class GameMode {
+import entity.Ball;
+import entity.BricksMap;
+import entity.Paddle;
+
+import java.util.ArrayList;
+
+public abstract class GameMode {
+    public BricksMap bricksMap;
+    public ArrayList<Ball> balls;
+    public Paddle paddle;
+
+    public abstract void create();
+    public abstract void update();
+    public abstract void render();
+    public abstract void draw();
 }
