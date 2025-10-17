bb8cdc8696c3ea01c64242485346207b72cedaba
QLinhhhh <24020201@vnu.edu.vn>
2025-10-17 20:05:09 +0700

    fix: expandPaddle with wall

--- core/src/main/java/entity/Effect/ExpandEffect.java
+++ core/src/main/java/entity/Effect/ExpandEffect.java
@@ -2,6 +2,8 @@ package entity.Effect;
 
 import entity.Paddle;
 import entity.TextureManager;
+import entity.BricksMap;
+import com.badlogic.gdx.Gdx;
 
 public class ExpandEffect extends EffectItem {
     private static final float EFFECT_DURATION = 5.0f;
@@ -15,8 +17,16 @@ public class ExpandEffect extends EffectItem {
     public void applyEffect() {
         if (paddle != null) {
             paddle.activateExpand(EFFECT_DURATION);
-        }
+            float screenWidth = Gdx.graphics.getWidth();
+            if (paddle.getX() + paddle.getWidth() > screenWidth - BricksMap.xBeginCoord) {
+                float newX = screenWidth - BricksMap.xBeginCoord - paddle.getWidth();
+                paddle.setX(newX);
+            }
 
+            if (paddle.getX() < BricksMap.xBeginCoord) {
+                paddle.setX(BricksMap.xBeginCoord);
+            }
+        }
         this.setDestroyed(true);
     }
 }
