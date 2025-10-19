7167af7b8eeb3ef9be469358fbf41f30478015bd
hieucoding123 <phamhieu211206@gmail.com>
2025-10-19 16:08:24 +0700

    feat(infinitemode): add balls and paddle

--- core/src/main/java/entity/gamemode/InfiniteMode.java
+++ core/src/main/java/entity/gamemode/InfiniteMode.java
@@ -3,17 +3,24 @@ package entity.gamemode;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.main.Game;
+import entity.Ball;
 import entity.BricksMap;
+import entity.Paddle;
 import entity.TextureManager;
 
 import java.util.ArrayList;
 
 public class InfiniteMode extends GameMode {
     private final ArrayList<BricksMap> bricksMaps;
+    private final ArrayList<Ball> balls;
+    boolean flowPaddle = true;      // Ball follow paddle
+    private Paddle paddle;
     private BricksMap currentMap;
 
     public InfiniteMode() {
         super();
+        balls = new ArrayList<>();
         bricksMaps = new ArrayList<>();
         create();
     }
@@ -25,11 +32,31 @@ public class InfiniteMode extends GameMode {
             bricksMaps.add(new BricksMap(mapPath));
         }
         currentMap =  bricksMaps.get(0);
+        paddle = new Paddle(Game.SCREEN_WIDTH / 2f - 48, 50, TextureManager.paddleTexture);
+        balls.add(new Ball(paddle.getX() + paddle.getWidth() / 2f - 12,
+            paddle.getY() + paddle.getHeight(),
+            TextureManager.ballTexture,
+            2.0f)
+        );
     }
 
     @Override
     public void update() {
         currentMap.update();
+        paddle.update();
+        if (balls.isEmpty()) this.reset();
+
+        if (flowPaddle) {       // follow paddle
+            balls.get(0).setX(paddle.getX() + (paddle.getWidth() / 2f) - balls.get(0).getWidth() / 2f);
+            balls.get(0).setY(paddle.getY() + paddle.getHeight());
+            balls.get(0).setAngle((float)Math.PI / 2f);
+        }
+        for (Ball ball : balls) {
+            ball.update();
+            ball.collisionWith(paddle);
+            ball.collisionWith(currentMap);
+        }
+        balls.removeIf(Ball::isDestroyed);
     }
 
     @Override
@@ -48,5 +75,7 @@ public class InfiniteMode extends GameMode {
     public void draw(SpriteBatch sp) {
         sp.draw(TextureManager.bgTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         currentMap.draw(sp);
+        for (Ball ball : balls) ball.draw(sp);
+        paddle.draw(sp);
     }
 }
