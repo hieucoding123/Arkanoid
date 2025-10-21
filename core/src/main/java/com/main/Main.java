5fc61671d26048a7c323bb528e1f0cc1488b834a
hieucoding123 <phamhieu211206@gmail.com>
2025-10-22 01:20:44 +0700

    refactor(main): call game handle input and update method

--- core/src/main/java/com/main/Main.java
+++ core/src/main/java/com/main/Main.java
@@ -30,6 +30,8 @@ public class Main extends ApplicationAdapter {
     }
     @Override
     public void render() {
+        game.handleInput();
+        game.update();
         game.render();
     }
 
