c0471ef71dd6ddab7acd7826b66cf2e660dc96e8
thdabezt <superthgamingvn@gmail.com>
2025-10-26 14:42:29 +0700

    feat(Control): Declared paddle movement flags

--- core/src/main/java/com/main/input/IngameInputHandler.java
+++ core/src/main/java/com/main/input/IngameInputHandler.java
@@ -4,4 +4,11 @@ import com.badlogic.gdx.InputAdapter;
 
 public class IngameInputHandler extends InputAdapter {
 
+    //Paddles Movements
+    private boolean paddle1Left = false;
+    private boolean paddle1Right = false;
+    private boolean paddle2Left = false;
+    private boolean paddle2Right = false;
+
+
 }
