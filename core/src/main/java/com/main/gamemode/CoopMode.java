9a4bb8259a41215172d8a354d1a79a09495b1cf5
thdabezt <superthgamingvn@gmail.com>
2025-10-26 13:22:57 +0700

    feat(Coop): Add ball follow paddle 1

--- core/src/main/java/com/main/gamemode/CoopMode.java
+++ core/src/main/java/com/main/gamemode/CoopMode.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 public class CoopMode extends GameMode{
     private final ArrayList<BricksMap> bricksMaps;
     private final ArrayList<Ball> balls;
-    boolean flowPaddle = true;      // Ball follow paddle
+    boolean followPaddle = true;      // Ball follow paddle
     private Paddle paddle1;
     private Paddle paddle2;
     private BricksMap currentMap;
@@ -90,5 +90,13 @@ public class CoopMode extends GameMode{
             this.lives--;
             //this.reset();
         }
+
+        if(followPaddle) {
+            balls.get(0).setX(paddle1.getX() + paddle1.getWidth() / 2f);
+            balls.get(0).setY(paddle1.getY() + paddle1.getHeight());
+            balls.get(0).setAngle((float)Math.PI / 2f);
+        }
+
+
     }
 }
