1c61ef350f56158e3a24ada69ccf6d1864ce9d50
thdabezt <superthgamingvn@gmail.com>
2025-10-26 13:13:13 +0700

    feat(Coop): added declarations

--- core/src/main/java/com/main/gamemode/CoopMode.java
+++ core/src/main/java/com/main/gamemode/CoopMode.java
@@ -1,5 +1,43 @@
 package com.main.gamemode;
 
-public class CoopMode extends GameMode{
+import entity.Effect.EffectFactory;
+import entity.GameScreen;
+import entity.Player;
+import entity.ScoreManager;
+import entity.object.Ball;
+import entity.object.Paddle;
+import entity.object.brick.BricksMap;
 
+import java.util.ArrayList;
+
+public class CoopMode extends GameMode{
+    private final ArrayList<BricksMap> bricksMaps;
+    private final ArrayList<Ball> balls;
+    boolean flowPaddle = true;      // Ball follow paddle
+    private Paddle paddle1;
+    private Paddle paddle2;
+    private BricksMap currentMap;
+    private ScoreManager scoreManager;
+    GameScreen gameScreen;
+    private EffectFactory effectFactory;
+    private int levelNumber;
+    private int mapIndex;
+    private int lives;
+    private double timePlayed;
+    private boolean start = false;
+
+    public CoopMode(Player player, ScoreManager scoreManager, GameScreen gameScreen, int levelNumber) {
+        super();
+        balls = new ArrayList<>();
+        bricksMaps = new ArrayList<>();
+
+        this.setPlayer(player);
+        this.setEnd(false);
+        this.scoreManager = scoreManager;
+        this.gameScreen = gameScreen;
+        this.effectFactory = new EffectFactory();
+        this.levelNumber = levelNumber;
+        this.timePlayed = 0.0f;
+        create();
+    }
 }
