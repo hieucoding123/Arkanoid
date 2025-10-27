692d1dc1d8ed684ac6125e82ebaf714eb2832d1d
thdabezt <24020165@vnu.edu.vn>
2025-10-27 14:01:16 +0700

    feat(UI): added Return buttons

--- core/src/main/java/Menu/leaderboard/LeaderBoard.java
+++ core/src/main/java/Menu/leaderboard/LeaderBoard.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+//import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.main.GameState;
 import com.main.Main;
