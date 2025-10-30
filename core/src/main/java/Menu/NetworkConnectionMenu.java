16bdd49b553ff47fc5a08d350c8de5ad789a9a41
hieucoding123 <phamhieu211206@gmail.com>
2025-10-30 17:45:31 +0700

    feat(networkconnectionmenu): display IPs discovered by send UDP

--- core/src/main/java/Menu/NetworkConnectionMenu.java
+++ core/src/main/java/Menu/NetworkConnectionMenu.java
@@ -1,5 +1,6 @@
 package Menu;
 
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -8,33 +9,29 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.esotericsoftware.kryonet.Client;
 import com.main.GameState;
 import com.main.Main;
-import com.main.network.IPDisplayDialog;
 import com.main.network.NetworkProtocol;
 import com.main.network.NetworkUtils;
 import entity.Player;
 
+import java.net.InetAddress;
+import java.util.ArrayList;
+
 public class NetworkConnectionMenu extends  UserInterface{
-    private TextField textField;
+    private List<String> serversList;
+    private ScrollPane serversListScrollPane;
+    private ArrayList<InetAddress> foundServers;
+
     private Label statusLabel;
-    private Label serverIPLabel;
-    private TextButton showAllIPsButton;
     private boolean isHosting;
 
     public NetworkConnectionMenu(Main main, Player player) {
         super(main, player);
-    }
-
-    private void showIPDialog(Skin skin) {
-        IPDisplayDialog dialog = new IPDisplayDialog("Server IP Information", skin);
-        dialog.show(this.getStage());
     }
 
     @Override
@@ -48,7 +45,7 @@ public class NetworkConnectionMenu extends  UserInterface{
         this.setFont(new BitmapFont(Gdx.files.internal("ui/F_Retro.fnt")));
         this.getFont().getData().setScale(1);
 
-        this.setSkin(new Skin(Gdx.files.internal("ui-skin/ui-skin.json")));
+        this.setSkin(new Skin(Gdx.files.internal("ui2/ui2.json")));
         this.getSkin().add("default-font", this.getFont());
 
         Gdx.input.setInputProcessor(this.getStage());
@@ -71,6 +68,7 @@ public class NetworkConnectionMenu extends  UserInterface{
 
         Table buttonTable = new Table();
 
+        // HOST BUTTON
         TextButton hostButton = new TextButton("Host Game", this.getSkin());
         hostButton.addListener(new ClickListener() {
             @Override
@@ -79,22 +77,18 @@ public class NetworkConnectionMenu extends  UserInterface{
             }
         });
 
-        serverIPLabel = new Label("", this.getSkin());
-        serverIPLabel.setColor(Color.GREEN);
-        serverIPLabel.setFontScale(1.0f);
-        serverIPLabel.setVisible(false);        // invisible first
+        serversList = new List<>(this.getSkin());
+        serversListScrollPane = new ScrollPane(serversList, this.getSkin());
+        serversListScrollPane.setFadeScrollBars(false);
 
-        showAllIPsButton = new TextButton("Show All Network IPs", this.getSkin());
-        showAllIPsButton.addListener(new ClickListener() {
+        // REFRESH BUTTON
+        TextButton refreshButton = new TextButton("Refresh List", this.getSkin());
+        refreshButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                showIPDialog(getSkin());
+                discoverServers();
             }
         });
-        showAllIPsButton.setVisible(false);
-
-        this.textField = new TextField("", this.getSkin());
-        textField.setMessageText("Enter server IP");
 
         TextButton joinButton = new TextButton("Join Game", this.getSkin());
         joinButton.addListener(new ClickListener() {
@@ -117,32 +111,64 @@ public class NetworkConnectionMenu extends  UserInterface{
                 main.setGameState(GameState.SELECT_MODE);
             }
         });
-        buttonTable.add(hostButton).padBottom(40).padTop(30).row();
-        buttonTable.add(serverIPLabel).padBottom(40).row();
-        buttonTable.add(showAllIPsButton).width(250).height(40).padBottom(20).row();
-        buttonTable.add(textField).padBottom(40).padTop(30).row();
+        buttonTable.add(hostButton).padBottom(20).padTop(30).row();
+        buttonTable.add(serversListScrollPane).width(300).height(150).padBottom(10).row();
+        buttonTable.add(refreshButton).padBottom(20).row();
         buttonTable.add(joinButton).padBottom(40).padTop(30).row();
         buttonTable.add(backButton).padBottom(40).padTop(30).row();
 
         mainTable.add(buttonTable);
     }
 
+    // Discovering servers by send UDP
+    private void discoverServers() {
+        statusLabel.setText("Discovering Servers...");
+        foundServers.clear();
+        serversList.clearItems();
+
+        new Thread(() -> {
+            // Client for discovering
+            Client tempClient =  new Client();
+            tempClient.start();
+            // Discovering
+            java.util.List<InetAddress> hosts = tempClient.discoverHosts(NetworkProtocol.UDP_PORT, 5000);
+            tempClient.stop();
+            tempClient.close();
+
+            // Update UI
+            Gdx.app.postRunnable(() -> {
+                if (hosts.isEmpty()) {
+                    statusLabel.setText("No Servers Found. Host one!");
+                } else {
+                    statusLabel.setText("Found " + hosts.size() + " server(s).");
+                    ArrayList<String> displayItems = new ArrayList<>();
+                    for (InetAddress host : hosts) {
+                        String IP = host.getHostAddress();
+                        foundServers.add(host);
+                        displayItems.add(IP);
+                    }
+                    serversList.setItems(displayItems.toArray(new String[0]));
+                }
+            });
+        }).start();
+    }
+
     // Join game -> Client
     private void joinGame() {
-        String severIP = textField.getText();
-        if (severIP.isEmpty()) {
-            statusLabel.setText("Please enter server IP");
+        String selectedIP = serversList.getSelected();
+        if (selectedIP == null) {
+            statusLabel.setText("Please Select Server!");
             return;
         }
 
-        statusLabel.setText("Connecting to " + severIP + " sever");
+        statusLabel.setText("Connecting to " + selectedIP + " sever");
 
         new Thread(() -> {
             try {
                 Thread.sleep(500);      // Connection delay
                 Gdx.app.postRunnable(() -> {
                     statusLabel.setText("Connected! Staring game...");
-                    getMain().startNetworkGame(severIP, false);
+                    getMain().startNetworkGame(selectedIP, false);
                 });
             } catch (Exception e) {
                 Gdx.app.postRunnable(() -> {
@@ -162,11 +188,6 @@ public class NetworkConnectionMenu extends  UserInterface{
                 String localIP = NetworkUtils.getLocalIpAddress();
                 Gdx.app.postRunnable(() -> {
                     statusLabel.setText("Sever started! Waiting for players...");
-
-                    serverIPLabel.setText("Your Server IP: " + localIP + "\nPort: " + NetworkProtocol.TCP_PORT);
-                    serverIPLabel.setVisible(true);
-
-                    showAllIPsButton.setVisible(true);
                     this.getMain().startNetworkGame(localIP, true);
                 });
             } catch (Exception e) {
