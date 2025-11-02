a5a01826bbf11c4e05bff85f38060fb44110750f
hkhoi23 <haikhoi2006@gmail.com>
2025-11-02 18:47:21 +0700

    refactor(vsmode) : change slow ball effect rate

--- core/src/main/java/com/main/gamemode/VsMode.java
+++ core/src/main/java/com/main/gamemode/VsMode.java
@@ -239,13 +239,13 @@ public class VsMode extends GameMode {
                         EffectItem newEffectItem = null;
                         if (ball.getLastHitBy() == 1) {
                             newEffectItem = effectFactory.tryCreateEffectItem(brick, paddle1, ball,
-                                0.02, 0.03, 0.03, 0.03, 0.04, 0.05, 0.06, 0.5, 0.08, 0.09);
+                                0.02, 0.03, 0.03, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09);
                             if (newEffectItem != null) {
                                 newEffectItem.setVelocity(0, -60f);
                             }
                         } else if (ball.getLastHitBy() == 2){
                             newEffectItem = effectFactory.tryCreateEffectItem(brick, paddle2, ball,
-                                0.02, 0.03, 0.03, 0.03, 0.04, 0.05, 0.06, 0.5, 0.08, 0.09);
+                                0.02, 0.03, 0.03, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09);
                             if (newEffectItem != null) {
                                 newEffectItem.setVelocity(0, 60f);
                             }
